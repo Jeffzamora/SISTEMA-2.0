4a2ece1a7a8a2be3cb6d9771949de256b58ad088
Jeffzamora <jeffzamorachiqui97@gmail.com>
2023-07-24 14:50:45 -0600

    Agrega view

--- view/MainNav/nav.php
+++ view/MainNav/nav.php
@@ -45,17 +45,46 @@
                         </a>
                     </li>
 
-                    <li class="blue-dirty">
-                        <a href="..\NuevaRemision\">
-                            <span class="glyphicon glyphicon-list-alt"></span>
-                            <span class="lbl">Nueva Remision</span>
-                        </a>
-                    </li>
+                    <li class="green with-sub">
+	                        <span>
+	                            <i class="font-icon font-icon-widget"></i>
+	                                <span class="lbl">Remisiones</span>
+	                        </span>
+	                    <ul>
+                            <li class="blue-dirty">
+                                <a href="..\NuevaRemision\">
+                                    <span class="glyphicon glyphicon-list-alt"></span>
+                                    <span class="lbl">Nueva Remision</span>
+                                </a>
+                            </li>
+
+                            <li class="blue-dirty">
+                                <a href="..\PreRemision\">
+                                    <span class="glyphicon glyphicon-list"></span>
+                                    <span class="lbl">Remision en Sucursal</span>
+                                </a>
+                            </li>
+
+                            <li class="blue-dirty">
+                                <a href="..\ConsultarRemision\">
+                                    <span class="glyphicon glyphicon-check"></span>
+                                    <span class="lbl">Consultar Remision</span>
+                                </a>
+                            </li>
+	                    </ul>
+	                </li>
 
                     <li class="blue-dirty">
                         <a href="..\NuevaCaja\">
                             <span class="glyphicon glyphicon-folder-close"></span>
                             <span class="lbl">Nueva Caja</span>
+                        </a>
+                    </li>
+
+                    <li class="blue-dirty">
+                        <a href="..\NuevoExpediente\">
+                            <span class="glyphicon glyphicon-folder-open"></span>
+                            <span class="lbl">Nuevo Expediente</span>
                         </a>
                     </li>
 
@@ -70,20 +99,6 @@
                         <a href="..\Sucursal\">
                             <span class="glyphicon glyphicon-globe"></span>
                             <span class="lbl">Sucursal</span>
-                        </a>
-                    </li>
-                    
-                    <li class="blue-dirty">
-                        <a href="..\PreRemision\">
-                            <span class="glyphicon glyphicon-list"></span>
-                            <span class="lbl">Remision en Sucursal</span>
-                        </a>
-                    </li>
-
-                    <li class="blue-dirty">
-                        <a href="..\ConsultarRemision\">
-                            <span class="glyphicon glyphicon-check"></span>
-                            <span class="lbl">Consultar Remision</span>
                         </a>
                     </li>
                 </ul>
